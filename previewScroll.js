1447ebf3beadac533b98f7bcd4380940938431c4
Eddie Flores <eddflrs@gmail.com>
2013-11-15 15:53:28 -0500

    Some serious refactoring.

--- previewScroll.js
+++ previewScroll.js
@@ -3,148 +3,191 @@
 
 (function () {
 
-  var splice = Array.prototype.splice;
+  var noop, slice, query, queryAll, extend, PreviewScroll;
 
-  var defaultOpts = {
-    pause_time: 300
+  /* Some helpers */
+
+  noop = function () {};
+
+  splice = Array.prototype.splice;
+
+  query = function (q) {
+    return document.querySelector.call(document, q);
   };
 
-  var bind = function (fn, ctx) {
-      return function () {
-        fn.apply(ctx, arguments);
-      };
+  queryAll = function (q) {
+    return document.querySelectorAll.call(document, q);
   };
 
-  /**
-   * @constructor
-   * Creates an instance of {PreviewScroll}
-   */
-  var PreviewScroll = function (containerId, opts) {
-    this.ul = document.querySelector(containerId);
-
+  extend = function (baseObj, childObj) {
     var k;
     for (k in opts) {
       if (opts.hasOwnProperty(k)) {
         defaultOpts[k] = opts[k];
       }
     }
+  }
+
+  /* Overrideable defaults */
+
+  var defaultOpts = {
+    pause_time: 300
+  };
+
+  /**
+   * @constructor
+   * Creates an instance of {PreviewScroll}
+   */
+  PreviewScroll = function (listId, opts) {
+    this.el = listId;
+
+    if (opts) {
+      extend(defaultOpts, opts);
+    }
 
     this.createPreviewArea();
-    this.run();
+    this.setDomReferences();
+    this.positionPreviewArea();
+    this.ensureScrollBoundaries();
+    this.listenToScrollPauses();
   };
 
-  /**
-   * @public
-   * Registers an event handler that is called when an item is previewed.
-   */
-  PreviewScroll.prototype.onPreview = function () { };
+  PreviewScroll.prototype.onPreview = noop;
 
   /**
    * @private
-   * Creates the preview area in the DOM.
+   * Sets references to the DOM as instance properties.
+   */
+  PreviewScroll.prototype.setDomReferences = function () {
+    this.ul = query(this.el);
+    this.allItems = queryAll('li');
+    this.allItemsLen = this.allItems.length;
+    this.firstItem = this.allItems[0];
+    this.lastItem = this.allItems[this.allItemsLen - 1];
+    this.items = splice.call(this.allItems, 1, this.allItemsLen - 1),
+    this.item = this.items[0];
+    this.previewArea = query('.preview-area');
+  };
+
+  /**
+   * Creates the preview area and inserts it into the DOM.
    */
   PreviewScroll.prototype.createPreviewArea = function () {
-    var wrapDiv = document.createElement('div'),
+    var parentToUl,
+      wrappingDiv = document.createElement('div'),
       previewAreaDiv = document.createElement('div'),
-      clonedNode = this.ul.cloneNode(true);
+      tempUl = query(this.el),
+      clonedUlNode = tempUl.cloneNode(true);
 
-      previewAreaDiv.classList.add('preview-area');
+    previewAreaDiv.classList.add('preview-area');
 
-      wrapDiv.appendChild(previewAreaDiv);
-      wrapDiv.appendChild(clonedNode);
+    wrappingDiv.appendChild(previewAreaDiv);
+    wrappingDiv.appendChild(clonedUlNode);
 
-      var parentNode = this.ul.parentNode;
-      parentNode.replaceChild(wrapDiv, this.ul);
+    parentToUl = tempUl.parentNode;
+    parentToUl.replaceChild(wrappingDiv, tempUl);
+
+    this.ul = tempUl;
   };
 
   /**
    * @private
-   * TODO Break up this run method.
+   * Positions the preview area to the vertical center of the ul.
    */
-  PreviewScroll.prototype.run = function () {
-    var item, self = this, ul = this.ul,
-      allItems = document.querySelectorAll('li'),
-      firstItem = allItems[0],
-      lastItem = allItems[allItems.length - 1],
-      allItemsLen = allItems.length,
-      items = splice.call(allItems, 1, allItemsLen - 1);
 
-    item = items[0];
-    ul = document.querySelector('ul');
+  PreviewScroll.prototype.positionPreviewArea = function () {
+    var ul = this.ul,
+      item = this.item,
+      ulHeight = ul.clientHeight,
+      previewArea = document.querySelector('.preview-area'),
+      midDistance;
 
-    /* Position the preview area */
-
-    var ulHeight = ul.clientHeight;
-    var previewArea = document.querySelector('.preview-area');
     previewArea.style.height = item.offsetHeight + 'px';
 
-    var previewAreaTop = (ulHeight - previewArea.offsetHeight) / 2;
-    var midDistance = ul.offsetTop + previewAreaTop;
+    this.previewAreaTop = (ulHeight - previewArea.offsetHeight) / 2;
+
+    midDistance = ul.offsetTop + this.previewAreaTop;
     previewArea.style.top = midDistance + 'px';
+  };
 
-    /* Ensure scroll boundaries do not exceed first and last items */
+  /**
+   * @private
+   * Ensure scroll boundaries do not exceed first and last items.
+   */
+  PreviewScroll.prototype.ensureScrollBoundaries = function () {
+    var previewAreaTop = this.previewAreaTop,
+      firstItem = this.firstItem,
+      lastItem = this.lastItem,
+      boundaryOffsetFirstItem = previewAreaTop - firstItem.offsetHeight,
+      boundaryOffsetLastItem = previewAreaTop - lastItem.offsetHeight;
 
-    var boundaryOffsetFirstItem = previewAreaTop - firstItem.offsetHeight;
-    var boundaryOffsetLastItem = previewAreaTop - lastItem.offsetHeight;
     firstItem.style.marginTop =   boundaryOffsetFirstItem + 'px';
     lastItem.style.marginBottom =  boundaryOffsetLastItem + 'px';
+  };
 
+  /**
+   * @private
+   * Determines which element is inside the preview area.
+   * Returns {Element} the item being previewed.
+   */
+  PreviewScroll.prototype.findPreviewedItem = function () {
+    var i = 0, distToTarget, itemHeights = 0, targetElem, currElem, currStyle,
+      allItems = this.allItems,
+      allItemsLen = this.allItemsLen,
+      ul = this.ul;
 
-    /* Snap to preview position when the user touches/clicks away. */
+    distToTarget = ul.scrollTop + (ul.offsetHeight / 2);
 
-    var snapToPreviewPosition = function () {
+    for (i; i < allItemsLen; i++) {
+      currElem = allItems[i];
+      currStyle = currElem.style;
 
-      console.log('Snap to preview psoition');
+      itemHeights += currElem.offsetHeight;
+      itemHeights += currStyle.marginTop ? parseInt(currStyle.marginTop, 10) : 0;
+      itemHeights += currStyle.marginBottom ? parseInt(currStyle.marginBottom, 10) : 0;
 
-      var animationId,
-        target = findPreviewedItem(),
-        targetTop = target.offsetTop - ul.scrollTop,
-        prevAreaTop = previewArea.offsetTop - ul.offsetTop,
-        compensate = targetTop - prevAreaTop,
-        cycleCount = 0;
+      if (distToTarget < itemHeights) {
+          targetElem = currElem;
+          break;
+      }
+    }
 
-      var scrollAnimation = function () {
-        if (cycleCount >= Math.abs(compensate)) {
-          cancelAnimationFrame(animationId);
-          return;
-        }
-        ul.scrollTop += (compensate < 0) ? -1 : 1;
-        cycleCount += 1;
-        animationId = requestAnimationFrame(scrollAnimation);
-      };
+    return targetElem;
+  };
 
+  /**
+   * @private
+   * Snaps the previewed item within the bounds of the preview area.
+   */
+  PreviewScroll.prototype.snapToPreviewArea = function () {
+    var animationId,
+      self = this,
+      target = self.findPreviewedItem(),
+      targetTop = target.offsetTop - self.ul.scrollTop,
+      prevAreaTop = self.previewArea.offsetTop - self.ul.offsetTop,
+      compensate = targetTop - prevAreaTop,
+      cycleCount = 0;
+
+    var scrollAnimation = function () {
+      if (cycleCount >= Math.abs(compensate)) {
+        cancelAnimationFrame(animationId);
+        return;
+      }
+      self.ul.scrollTop += (compensate < 0) ? -1 : 1;
+      cycleCount += 1;
       animationId = requestAnimationFrame(scrollAnimation);
     };
 
-    /*
-     * Determines which element is inside the preview area.
-     * Returns {Element} the item being previewed.
-     */
-    var findPreviewedItem = function () {
-      var i = 0, distToTarget, itemHeights = 0, targetElem, currElem, currStyle;
+    animationId = requestAnimationFrame(scrollAnimation);
+  };
 
-      distToTarget = ul.scrollTop + (ul.offsetHeight / 2);
-
-      for (i; i < allItemsLen; i++) {
-        currElem = allItems[i];
-        currStyle = currElem.style;
-
-        itemHeights += currElem.offsetHeight;
-        itemHeights += currStyle.marginTop ? parseInt(currStyle.marginTop, 10) : 0;
-        itemHeights += currStyle.marginBottom ? parseInt(currStyle.marginBottom, 10) : 0;
-
-        if (distToTarget < itemHeights) {
-            targetElem = currElem;
-            break;
-        }
-      }
-
-      return targetElem;
-    };
-
-    /* Detect when the user pauses or stops scrolling */
-
-    var timeoutId, prevScroll;
+  /**
+   * @private
+   * Detects when the user pauses/stops scrolling and snaps to the
+   * preview area.
+   */
+  PreviewScroll.prototype.listenToScrollPauses = function () {
+    var self = this, ul = this.ul, timeoutId, prevScroll;
 
     ul.addEventListener('scroll', function (evt) {
       var currScroll = ul.scrollTop;
@@ -160,9 +203,11 @@
 
       clearTimeout(timeoutId);
       timeoutId = setTimeout(function () {
+        var targetItem;
         if (prevScroll === currScroll) {
-          self.onPreview(findPreviewedItem());
-          snapToPreviewPosition();
+          targetItem = self.findPreviewedItem();
+          self.onPreview(targetItem);
+          self.snapToPreviewArea();
         }
       }, defaultOpts.pause_time);
 
@@ -170,10 +215,15 @@
     });
   };
 
+  /**
+   * @public
+   * Updates the position of the preview area and scroll boundaries.
+   * Should be called whenever the height of the ul changes.
+   */
   PreviewScroll.prototype.updateLayout = function () {
-    this.run();
+    this.positionPreviewArea();
+    this.ensureScrollBoundaries();
   };
 
   this.PreviewScroll = PreviewScroll;
-
-} ());
+}());
